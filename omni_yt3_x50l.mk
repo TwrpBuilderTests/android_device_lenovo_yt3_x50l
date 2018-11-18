ee0eaaddeffce1ead78ed7c149c51e45b0f84404
dark98 <darkaidz98@gmail.com>
2018-11-18 03:49:13 +0000

    Initial commit

---
+++ omni_yt3_x50l.mk
@@ -0,0 +1,22 @@
+#
+# Copyright (C) 2018 The TwrpBuilder Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+PRODUCT_DEVICE := yt3_x50l
+PRODUCT_NAME := omni_yt3_x50l
+PRODUCT_BRAND := lenovo
+PRODUCT_MODEL := Lenovo YT3-X50L
+PRODUCT_MANUFACTURER := lenovo
